56ad4fd5fb5d5a74049fd652230733e6e1d3c551
unknown <agudarustefan@gmail.com>
2022-01-19 21:28:57 +0200

    curs02

---
+++ 02-pythonic-hello-world/data_structures.py
@@ -0,0 +1,22 @@
+lista = [8,2,-5, "string", 2.7]
+#print(type(lista))
+#print(lista[3])
+#print(lista[5]) #eroare index nu exista
+#print(lista[1:])
+#print(lista[1:3])
+#print(lista[5])
+#print(lista[0:5:2]) #de la 0 la 5 din 2 in 2
+#print(lista[-1]) #ultimul element din lista
+#print(lista[-1::-1])
+#lista.append("mere")
+#lista[3]= "mere"
+#print(lista.index(-5))
+#lista.insert(3, "element nou")
+#lista.remove(8) #remove element
+#lista.pop(1) #remove index
+#lista.clear() #remove all element
+#lista.reverse() #inversare elemente
+#lista.sort() # sorteaza elementele <
+#lista_stirnguri = ['8', '2', "21"]
+#lista_stirnguri.sort()
+#print(lista_stirnguri)
