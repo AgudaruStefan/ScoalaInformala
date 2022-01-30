296661459ed1011eca5da4915383e76c030f5cfb
unknown <agudarustefan@gmail.com>
2022-01-31 00:26:58 +0200

    curs03

---
+++ 03-data-structure/programare_conditionala_looping.py
@@ -0,0 +1,45 @@
+# daca conditie :
+#   print('adevarat')
+# daca alta conditie:
+#   print('nici asceasta nu e adevarata')
+# altfel:
+# print("afisez doar daca "conditie si "alta conditie" nu sunt adevarate
+
+
+#my_var = 5
+#if my_var < 6""
+#    print('set instructiuni #1')
+#elif myvar < 10:
+#    print("set intructiuni #2")
+#else:
+#    print("set intructiuni #3")
+# print("a iesit")
+
+
+
+# my_var = 7
+# mesaj = "Set instructiuni #3"
+# if my_var < 6:
+#         mesaj = 'set instructiuni #1'
+# elif myvar < 10:
+#           mesaj = "set intructiuni #2"
+
+# var = 1
+# while var < 10:
+#     var = var + 1
+#     print('bloc de instructiuni', var)
+#     var += 1
+#     break
+
+string = "Ana are mere"
+lista = [1, 2, 3, 'a']
+ for i, v in enumerate(lista):
+     print(i, '=>', v)
+ for variabila_temporara in range(0, len(lista)):
+     print(lista[variabila_temporara])
+ print(variabila_temporara, "<<")
+dictionar = {"1": 'unu', "2": 'doi', "3": 'trei'}
+for item, value in dictionar.items():
+    print(item, value)
+for item in dictonar.keys():
+    print(item, dictionar.get(item))
